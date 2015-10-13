aea8eb562318e7f9de5d51a1ba0cb0c05efe68a1
Mayank Patel <mayankpatel@browseinfo.in>
2015-10-13 11:50:46 +0530

    Changes in Product price and taxes in invoice line

--- custom_sale.py
+++ custom_sale.py
@@ -28,47 +28,52 @@ class sale_order(osv.osv):
     _inherit = "sale.order"
     
     def action_wait(self, cr, uid, ids, context=None):
-        tax_list = []
         context = context or {}
         Invoice_pool = self.pool.get('account.invoice')
         Inv_line_pool = self.pool.get('account.invoice.line')
         Journal_pool = self.pool.get('account.journal')
+        account_pool = self.pool.get('account.account')
         for o in self.browse(cr, uid, ids):
             if o.warehouse_id.partner_id.company_id.id != o.warehouse_id.company_id.id:
-                if o.warehouse_id.partner_id and o.warehouse_id.partner_id.is_company == True:
-                    account_id = o.warehouse_id.partner_id.property_account_payable.id
-                    payment_term = o.warehouse_id.partner_id.property_supplier_payment_term.id
-                elif o.warehouse_id.partner_id and o.warehouse_id.partner_id.is_company == False:
-                    account_id = o.warehouse_id.partner_id.parent_id.property_account_payable.id
-                    payment_term = o.warehouse_id.partner_id.parent_id.property_supplier_payment_term.id
+#                if o.warehouse_id.partner_id and o.warehouse_id.partner_id.is_company == True:
+#                    account_id = o.warehouse_id.partner_id.property_account_payable.id
                     
-                journal_id = Journal_pool.search(cr, uid, [('type', '=', 'purchase'), ('company_id', '=', o.warehouse_id.partner_id.company_id.id)])
+#                    payment_term = o.warehouse_id.partner_id.property_supplier_payment_term.id
+#                elif o.warehouse_id.partner_id and o.warehouse_id.partner_id.is_company == False:
+#                    account_id = o.warehouse_id.partner_id.parent_id.property_account_payable.id
+                    
+ #                   payment_term = o.warehouse_id.partner_id.parent_id.property_supplier_payment_term.id
                 
+                account_id = account_pool.search(cr, uid, [('company_id', '=', o.warehouse_id.company_id.id), ('type', '=', 'payable')])
+                
+                journal_id = Journal_pool.search(cr, uid, [('type', '=', 'purchase'), ('company_id', '=', o.warehouse_id.company_id.id)])
+                print '\n acc and journal',account_id,journal_id
                 sup_inv_vals = {
                     'partner_id': o.warehouse_id.partner_id.id,
                     'currency_id': o.pricelist_id.currency_id.id,
                     'company_id': o.warehouse_id.company_id.id,
-                    'account_id': account_id or False,
+                    'account_id': account_id and account_id[0] or False,
                     'journal_id': journal_id and journal_id[0] or False,
                     'user_id': o.user_id.id,
                     'origin': o.name,
                     'type': 'in_invoice',
-                    'payment_term': payment_term,
+#                    'payment_term': payment_term,
                 }
                 sup_inv_id = Invoice_pool.create(cr, uid, sup_inv_vals)
                 print '\n supplier inv',sup_inv_id,sup_inv_vals,journal_id
                 for sale_line in o.order_line:
-                    for tax in sale_line.tax_id:
+                    tax_list = []
+                    for tax in sale_line.product_id.supplier_taxes_id:
                         tax_list.append(tax.id)
                     inv_line_vals = {
                         'product_id': sale_line.product_id.id,
                         'name': sale_line.name,
                         'quantity': sale_line.product_uom_qty,
                         'uos_id': sale_line.product_uom.id,
-                        'account_id': account_id or False,
-                        'price_unit': sale_line.price_unit,
+                        'account_id': account_id and account_id[0] or False,
+                        'price_unit': sale_line.product_id.standard_price,
                         'invoice_id': sup_inv_id,
-                        'discount': sale_line.discount,
+#                        'discount': sale_line.discount,
                         'invoice_line_tax_id': [(6, 0, tax_list)],
                     }
                     print '\n supplier inv line',inv_line_vals
